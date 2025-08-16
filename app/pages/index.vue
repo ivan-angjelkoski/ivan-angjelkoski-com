1211853f6c789367cd4429f6f1df461db81f46e0
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2025-08-16 20:03:07 +0200

    chore: homepage init

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,15 @@
 
 <template>
   <div>
-    <h1>Home</h1>
+    <div class="max-w-3xl mx-auto w-full flex flex-col items-center mt-10">
+      <img
+        src="/ivan_angjelkoski.jpeg"
+        alt="Ivan Angjelkoski"
+        class="w-40 h-40 rounded-full"
+      />
+
+      <h2 class="text-2xl font-bold">Ivan Angjelkoski</h2>
+      <h3 class="text-zinc-500">Frontend Developer - Injective Labs</h3>
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxtjs/color-mode"],
+  colorMode: {
+    fallback: "dark",
+    storageKey: "ivan-angjelkoski-color-mode",
+    preference: "dark",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
